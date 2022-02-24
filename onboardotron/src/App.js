b20d4f22bcb48bcd328a811d423992f1403017f9
LonnieFowler <97270531+LonnieFowler@users.noreply.github.com>
2022-02-24 00:41:18 -0700

    created forms for name, email and password. created a ToS checkbox and a submit button

--- onboardotron/src/App.js
+++ onboardotron/src/App.js
@@ -1,11 +1,28 @@
-
+import React, {useState} from 'react';
 import './App.css';
-import Form from './Components/Form.js'
+import Form from './Components/Form'
+
+const initialFormValues = {
+  username:'',
+  password:'',
+  email:'',
+  tos: false
+}
+
 
 function App() {
+  const [formValues, setFormValues] = useState(initialFormValues);
+
+  const handleSubmit = () => {
+     
+  }
+  
+  const handleChange =(name,value) => {
+    setFormValues({...formValues, [name]:value});
+  }
   return (
     <div className="App">
-      <Form />
+      <Form values={formValues} change={handleChange} />
     </div>
   );
 }
--- onboardotron/src/Components/Form.js
+++ onboardotron/src/Components/Form.js
@@ -2,8 +2,65 @@ import React from 'react';
 
 
 const Form= (props) => {
+    const {change,submit}= props;
+    const {username, email, password, tos,}= props.values;
+    
+    const onChange = (evt) => {
+        const {name, value, checked, type} = evt.target;
+        const newValue = type ==='checkbox' ? checked : value;
+        change(name, newValue)
+
+    }
+
+    const onSubmit = (evt) => {
+      evt.preventDefault();
+       submit(); 
+    }
     return (
-       <h1>LOLOLOLOLOL!</h1> 
+        <div>
+       <h1>LOLOLOLOLOL!</h1>
+       <form onSubmit = {onSubmit} >
+           <label>Name:
+               <input
+               type='text'
+               name='username'
+               value={username}
+               onChange={onChange}
+               />
+            </label>
+            <label>Email:
+                <input 
+                type='email'
+                name= 'email'
+                value={email}
+                onChange={onChange}
+                
+                />
+            </label>
+            <label>Password
+                <input
+                type='password'
+                name='password'
+                value={password}
+                onChange={onChange} 
+            
+            />
+            </label>
+            <label>Terms Of Service
+                <input 
+                type='checkbox'
+                name='tos'
+                checked={tos}
+                onChange={onChange}
+            />
+            </label>
+            
+                <input type='submit' value='Create A User Nao!' />
+            
+        </form> 
+
+
+       </div>
     )
 }
 
