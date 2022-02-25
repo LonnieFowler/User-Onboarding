001f809e4e490e35fdb04b189c706edfee823f90
LonnieFowler <97270531+LonnieFowler@users.noreply.github.com>
2022-02-25 00:31:19 -0700

    name,email and password tests

--- onboardotron/cypress/integration/form_test.js
+++ onboardotron/cypress/integration/form_test.js
@@ -7,11 +7,28 @@ describe('Form App', () => {
 
 
 //getters
-const 
+const nameInput = () => cy.get('input[name=username]') 
+const emailInput = () => cy.get('input[name=email]')
+const passInput = () => cy.get('input[name=password]')
+const tosInput = () => cy.get('input[name=tos]')
+const submitInput = () => cy.get('input[type=submit]')
 
+it('user inputs recieved', () => {
+    nameInput()
+    .should('have.value','')
+    .type('Lonnie')
+    .should('have.value','Lonnie')
 
+    emailInput()
+    .should('have.value','')
+    .type('lonnie@gmail.com')
+    .should('have.value','lonnie@gmail.com')
 
-
+    passInput()
+    .should('have.value','')
+    .type('mypassword')
+    .should('have.value','mypassword')
+})
 
 
 
